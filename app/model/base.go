23c4492c15f006bd84d22a66e19e1de63fa7a34b
haibo <2537070941@qq.com>
2018-01-11 20:10:11 +0800

    更新

---
+++ app/model/base.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+    "github.com/astaxie/beego"
+    Orm "github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql"
+
+    . "fmt"
+)
+
+var orm Orm.Ormer
+
+func Register() {
+    // db init
+    Orm.Debug, _ = beego.AppConfig.Bool("debug")
+    Orm.RegisterDriver("mysql", Orm.DRMySQL)
+    err := Orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("db_mysql"))
+    orm = Orm.NewOrm()
+    orm.Using("default")
+}
